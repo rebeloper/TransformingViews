a2544fc3fbed3d205ea10ed7d565514bc96aee5f
Alpar <Matyas>
2019-10-29 12:02:55 +0200

    Initial Commit

---
+++ TransformingViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TransformingViews
+//
+//  Created by Alpar Matyas on 29/10/2019.
+//  Copyright © 2019 Alpar Matyas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
